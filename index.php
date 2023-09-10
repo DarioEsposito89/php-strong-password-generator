968bda1d33040a669f7f0e2702d540544dd4142d
DarioEsposito89 <darioesposito.work@gmail.com>
2023-09-10 11:41:01 +0200

    Creato layout e form

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Strong Password Generator</title>
+</head>
+
+<body style="background-color: rgb(94, 132, 173);">
+
+    <div class="container text-center pt-5">
+        <h1 style="color: rgb(0, 0, 0)">Strong Password Generator</h1>
+
+        <h2 style="color: white">Genera una password sicura</h2>
+    </div>
+
+    <div class="container w-50 p-3 rounded mb-3" style="background-color: rgb(0, 50, 105); color: rgb(82, 141, 169);">
+        <p class="m-0"></p>
+    </div>
+
+    
+    <div class="container w-50 p-3 rounded" style="background-color: white">
+        <form action="" method="get">
+            <div class="row">
+
+                <div class="col-7">
+                    <span>Lunghezza password: </span>
+                </div>
+
+                <div class="col-5">
+                    <input class="w-50" type="number" name="passLength">
+                </div>
+            </div>
+
+            <button type="submit" class="btn btn-primary mt-3">Invia</button>
+
+        </form>
+
+    </div>
+</body>
+
+</html>
+
